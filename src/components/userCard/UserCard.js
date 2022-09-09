c98115d034acbb781755d8ebb7a5ae5ca9852c47
Tr1mp <@Tr1mp>
2022-09-09 04:12:56 +0300

    The user's personal card has been completed

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -1,29 +1,57 @@
 
 
+import { useEffect, useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import useAuthUser from '../../services/AuthUser';
+
 import "./userCard.scss";
 
 const UserCard = () => {
+
+    const {id} = useParams();
+    const {getData} = useAuthUser();
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        getData(`admin/users/${id}?fields=id,login,name,email,type`)
+            .then(setUser);
+        // eslint-disable-next-line
+    }, [id])
+
+
+    function renderItem(obj) {
+        const {id, name, $type, login, email} = obj;
+        return (
+            <div className="user__card">
+                <div className="user__card-round">
+                    <h3 className="user__card-round-id">
+                        {id}
+                    </h3>
+                </div>
+                <div className="user__card-wrapper">
+                    <h2 className="user__card-name">
+                        {name}                    
+                    </h2>
+                    <span>{$type}</span>
+                    <p className="user__card-text">
+                        <span className="user__card-text-span">login:</span> <br/> {login}
+                    </p>
+                    <p className="user__card-text">
+                        <span className="user__card-text-span">e-mail:</span> <br/> {email}
+                    </p>
+                </div>
+            </div>
+        )
+    }
+
+    const item = useMemo(() => renderItem(user),
+        
+        [user])
     return (
-        <div className="user__card">
-            <div className="user__card-round">
-                <h3 className="user__card-round-id">
-                    2-705
-                </h3>
-            </div>
-            <div className="user__card-wrapper">
-                <h2 className="user__card-name">
-                    John Smith<br/>
-                    
-                </h2>
-                <span>User</span>
-                <p className="user__card-text">
-                    <span>login:</span> some_login
-                </p>
-                <p className="user__card-text">
-                    <span>e-mail:</span> some_long@mail.com
-                </p>
-            </div>
-        </div>
+        <> 
+            {item}
+        </>
     )
 }
 
